878d15f8864a481fec811cd83f5a0b03fdbdb8de
swapnildss456 <swapnildss456@gmail.com>
2022-11-10 19:58:52 +0530

    git

--- git1.sh
+++ git1.sh
@@ -11,12 +11,6 @@ branch="main"
 git add .
 read -p "Enter your commit messgae: " commit
 git commit -m $commit
-git push -u $branch
+git push -u "$branch"
 echo -e "\e[31m code has been pushed. \e[0m"
 
-# git add .
-# read -p "Enter your commit message: " Commit_message
-# git commit -m "$Commit_message" 
-# git push
-# # git commit -m "auto commit" $1
-# # git push origin $branch
