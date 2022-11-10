49fdea0557192af9ac36b88daf420c6f94f9a839
swapnildss456 <swapnildss456@gmail.com>
2022-11-10 19:41:47 +0530

    git

--- git1.sh
+++ git1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#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
 
 #1. How do you set up a script to run every time a repository receives new commits through push?
 # 2. How do you find a list of files that have changed in a particular commit?
