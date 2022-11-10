e73513616dbb57b1242dcbbaf35570d5793f9248
swapnildss456 <swapnildss456@gmail.com>
2022-11-10 19:39:11 +0530

    git

--- git1.sh
+++ git1.sh
@@ -5,10 +5,9 @@
 
 #1. How do you set up a script to run every time a repository receives new commits through push?
 
-branch=main
 git add .
-# echo "Enter your commit message: " Commit_message
-# git commit -m "$Commit_message" 
-# git push
-git commit -m "auto commit" $1
-git push origin $branch
+read -p "Enter your commit message: " Commit_message
+git commit -m "$Commit_message" 
+git push
+# git commit -m "auto commit" $1
+# git push origin $branch
