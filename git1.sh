a68d9ae45ec7639b22074c00a28d168149d38a8f
swapnildss456 <swapnildss456@gmail.com>
2022-11-10 20:03:06 +0530

    git

--- git1.sh
+++ git1.sh
@@ -2,10 +2,6 @@
 #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
 
 #1. How do you set up a script to run every time a repository receives new commits through push?
-# 2. How do you find a list of files that have changed in a particular commit?
-
-#1. How do you set up a script to run every time a repository receives new commits through push?
-
 
 branch="main"
 git add .
@@ -14,3 +10,6 @@ git commit -m $commit
 git push -u "$branch"
 echo -e "\e[31m code has been pushed. \e[0m"
 
+
+# 2. How do you find a list of files that have changed in a particular commit?
+git log
